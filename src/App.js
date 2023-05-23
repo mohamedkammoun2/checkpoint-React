071c2b2d633991eb8a5badac56639b8b25686c0c
mohamedkammoun2 <moh.kammoun@gmail.com>
2023-05-23 22:29:14 +0100

    React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Button, Container, Form, Nav, Navbar, NavDropdown, Table } from 'react-bootstrap';
+import Navebare from './components/Navbare';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbare
+    
+<h1>
+Welcome to Tunisia by Med Kammoun 
+</h1>
+
+  
     </div>
   );
 }
